08cbe48264eb6eeb4bb7ef3ef2ff952143ffcbfc
jackyyang1121 <mingyongpig@gmail.com>
2025-05-20 03:27:22 +0800

    modify

--- backend/search/views.py
+++ backend/search/views.py
@@ -16,13 +16,14 @@ class SearchListView(generics.GenericAPIView):
         # 我在任何 view 裡都可以安全地使用 request.user 和 request.user.is_authenticated。
            user = request.user.username   #request.user可以直接使用，並抓到User物件內的資訊，User包含id、username、password、email等資訊
            #不管User是用預設模型還是自訂模型，只要在settings.py裡有AUTH_USER_MODEL，就可以用request.user來抓到User物件
-        query = request.GET.get('q')    #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的q，也就是searchParams中的q
-        public = str(request.GET.get('public')) != "0"   #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的public，也就是searchParams中的public
-        tag = request.GET.get('tag') or None    #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的tag，也就是searchParams中的tag
+        query = request.GET.get('q')    #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的q，也就是searchParams中的q，q在index.html中的search表單中有定義name="q"
+        public = str(request.GET.get('public')) != "0"   #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的public，也就是searchParams中的public，但是目前insex.html搜尋表單內我並沒有添加public這個參數所以目前?${searchParams}中沒有存取到public這個參數
+        tag = request.GET.get('tag') or None    #這邊去拿 `${baseEndpoint}/search/?${searchParams}`中的tag，也就是searchParams中的tag，但是目前insex.html搜尋表單內我並沒有添加tag這個參數所以目前?${searchParams}中沒有存取到tag這個參數
+        #因為我前端index.html搜尋表單內沒有添加name=tag和name=public這兩個參數，所以這邊的tag和public會是None，因為?${searchParams}沒辦法存取到這兩個參數
         if not query:
             return Response('', status=400)
         results = client.perform_search(query, tags=tag, user=user, public=public)
-        return Response(results)
+        return Response(results) #回傳API的結果
 
 class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
